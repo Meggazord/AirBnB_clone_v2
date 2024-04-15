0bcdf94f0ec6be00d4d313d45b43d7f29fd536b7
Meggazord <k_megahed@hotmail.com>
2024-04-15 15:31:12 +0200

    fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,6 +12,66 @@ env.key_filename = '/Users/Megahed/.ssh/id_rsa'
 
 def do_deploy(archive_path):
     """
+    Distributes an archive to the web servers and deploys it
+
+    Args:
+        archive_path (str): Path to the archive to deploy
+
+    Returns:
+        bool: True if all operations were done correctly, False otherwise
+    """
+    if not os.path.exists(archive_path):
+        return False
+
+    try:
+        archive_name = os.path.basename(archive_path)
+        archive_name_no_ext = os.path.splitext(archive_name)[0]
+        remote_tmp = '/tmp/'
+
+
+        put(archive_path, remote_tmp)
+
+    
+        run('mkdir -p /data/web_static/releases/{}/'
+            .format(archive_name_no_ext))
+
+
+        run('tar -xzf {}{}/ -C /data/web_static/releases/{}/'
+            .format(remote_tmp, archive_name, archive_name_no_ext))
+
+
+        run('rm {}{}'.format(remote_tmp, archive_name))
+
+
+        run('mv /data/web_static/releases/{}/web_static/* '
+            '/data/web_static/releases/{}/'
+            .format(archive_name_no_ext, archive_name_no_ext))
+
+
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(archive_name_no_ext))
+
+
+        run('rm -rf /data/web_static/current')
+
+
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
+            .format(archive_name_no_ext))
+
+        print("New version deployed!")
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
+
+if __name__ == "__main__":
+    do_deploy()
+
+
+
+#def do_deploy(archive_path):
+    """
     Distributes an archive to your web servers
 
     Args:
@@ -20,6 +80,7 @@ def do_deploy(archive_path):
     Returns:
         bool: True if all operations were successful, False otherwise
     """
+"""
     if not exists(archive_path):
         return False
     try:
@@ -39,3 +100,4 @@ def do_deploy(archive_path):
 
 if __name__ == "__main__":
     do_deploy("<path_to_archive>")
+"""
