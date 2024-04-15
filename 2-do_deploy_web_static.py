1c4cbe297e50b2dd022bdc91155b25daf01754df
Meggazord <k_megahed@hotmail.com>
2024-04-15 17:14:43 +0200

    fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -27,37 +27,20 @@ def do_deploy(archive_path):
         archive_name = os.path.basename(archive_path)
         archive_name_no_ext = os.path.splitext(archive_name)[0]
         remote_tmp = '/tmp/'
-
-
         put(archive_path, remote_tmp)
-
-    
         run('mkdir -p /data/web_static/releases/{}/'
             .format(archive_name_no_ext))
-
-
         run('tar -xzf {}{}/ -C /data/web_static/releases/{}/'
             .format(remote_tmp, archive_name, archive_name_no_ext))
-
-
         run('rm {}{}'.format(remote_tmp, archive_name))
-
-
         run('mv /data/web_static/releases/{}/web_static/* '
             '/data/web_static/releases/{}/'
             .format(archive_name_no_ext, archive_name_no_ext))
-
-
         run('rm -rf /data/web_static/releases/{}/web_static'
             .format(archive_name_no_ext))
-
-
         run('rm -rf /data/web_static/current')
-
-
         run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
             .format(archive_name_no_ext))
-
         print("New version deployed!")
         return True
 
@@ -69,9 +52,9 @@ if __name__ == "__main__":
     do_deploy()
 
 
-
+"""
 #def do_deploy(archive_path):
-    """
+"""    """
     Distributes an archive to your web servers
 
     Args:
