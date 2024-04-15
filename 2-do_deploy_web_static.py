4b24bc990decc9b5f3bf8d421239077642474720
Meggazord <k_megahed@hotmail.com>
2024-04-15 18:09:27 +0200

    fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,8 +8,6 @@ from os.path import exists
 import os
 
 env.hosts = ['18.207.2.191', '3.90.83.114']
-env.user = 'ubuntu'
-env.key_filename = '/Users/Megahed/.ssh/id_rsa'
 
 def do_deploy(archive_path):
     """
@@ -51,37 +49,3 @@ def do_deploy(archive_path):
 
 if __name__ == "__main__":
     do_deploy()
-
-
-"""
-#def do_deploy(archive_path):
-"""    """
-    Distributes an archive to your web servers
-
-    Args:
-        archive_path (str): Path to the archive file
-
-    Returns:
-        bool: True if all operations were successful, False otherwise
-    """
-"""
-    if not exists(archive_path):
-        return False
-    try:
-        put(archive_path, '/tmp')
-        archive_name = archive_path.split('/')[-1]
-        release_folder = '/data/web_static/releases/' + archive_name [:-4]
-        run('mkdir -p {}'.format(release_folder))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_name, release_folder))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(release_folder))
-        print("Successful deployment!")
-        return True
-    
-    except Exception as e:
-        print("Error:", e)
-        return False
-
-if __name__ == "__main__":
-    do_deploy("<path_to_archive>")
-"""
